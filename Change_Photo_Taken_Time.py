0f80816e56552651aaa6973250d27c11a8b1694e
dsouzawilbur <dsouza.wilbur@gmail.com>
2020-07-02 11:25:48 +0100

    Update Change_Photo_Taken_Time.py
    Offset GST to 4:00, to prevent photos showing up in the previous day.

--- Change_Photo_Taken_Time.py
+++ Change_Photo_Taken_Time.py
@@ -13,9 +13,9 @@ def absoluteFilePaths(directory):
 				month= match.group(2)
 				day = match.group(3)
 				exif_dict = piexif.load(fullPath)
-				exif_dict['Exif'][36867] = year+':'+month+':'+day+' 00:00:00'
-				exif_dict['Exif'][36868] = year+':'+month+':'+day+' 00:00:00'
-				exif_dict['0th'][306] = year+':'+month+':'+day+' 00:00:00'
+				exif_dict['Exif'][36867] = year+':'+month+':'+day+' 04:00:00'
+				exif_dict['Exif'][36868] = year+':'+month+':'+day+' 04:00:00'
+				exif_dict['0th'][306] = year+':'+month+':'+day+' 04:00:00'
 				exif_bytes = piexif.dump(exif_dict)
 				piexif.insert(exif_bytes, fullPath)
 				print("############################")
